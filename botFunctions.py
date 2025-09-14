8ac3ee009bcf4ec8e7c3d2543a8958eeda16c757
DanilBot-ui <grown.dk.up@gmail.com>
2025-09-14 10:08:33 -0700

    add Pycharm config

--- botFunctions.py
+++ botFunctions.py
@@ -1,13 +1,13 @@
-from main import *
-from winnerUtils import *
 import random
+from winnerUtils import check_winner_state
 
-def random_bot_move():
-    choices = [(r, c) for r in range(3) for c in range(3) if board[r][c] == '']
-    return random.choice(choices)
+def random_bot_move(board_state):
+    choices = [(r, c) for r in range(3) for c in range(3) if board_state[r][c] == '']
+    if choices:
+        return random.choice(choices)
+    return None, None
 
-
-def minimax(board_state, player, maximizing, bot_symbol):
+def minimax(board_state, bot_symbol, maximizing):
     opp = 'O' if bot_symbol == 'X' else 'X'
     result = check_winner_state(board_state)
     if result == bot_symbol:
@@ -24,7 +24,7 @@ def minimax(board_state, player, maximizing, bot_symbol):
             for c in range(3):
                 if board_state[r][c] == '':
                     board_state[r][c] = bot_symbol
-                    score, _ = minimax(board_state, player, False, bot_symbol)
+                    score, _ = minimax(board_state, bot_symbol, False)
                     board_state[r][c] = ''
                     if score > best_score:
                         best_score = score
@@ -37,33 +37,21 @@ def minimax(board_state, player, maximizing, bot_symbol):
             for c in range(3):
                 if board_state[r][c] == '':
                     board_state[r][c] = opp
-                    score, _ = minimax(board_state, player, True, bot_symbol)
+                    score, _ = minimax(board_state, bot_symbol, True)
                     board_state[r][c] = ''
                     if score < best_score:
                         best_score = score
                         best_move = (r, c)
         return best_score, best_move
 
-def hard_bot_move():
-    _, move = minimax([row[:] for row in board], bot_symbol, True, bot_symbol)
+def hard_bot_move(board_state, bot_symbol):
+    _, move = minimax([row[:] for row in board_state], bot_symbol, True)
     if move is None:
-        return random_bot_move()
+        return random_bot_move(board_state)
     return move
 
-
-def bot_move():
-    global current_player, game_over, winner
-    if game_over:
-        return
-    if current_player == bot_symbol:
-        if bot_difficulty == 'easy':
-            r, c = random_bot_move()
-        else:
-            r, c = hard_bot_move()
-        board[r][c] = bot_symbol
-        winner = check_winner()
-        if winner is not None:
-            game_over = True
-        else:
-            current_player = 'O' if current_player == 'X' else 'X'
-
+def bot_move(board_state, bot_symbol, bot_difficulty):
+    if bot_difficulty == 'easy':
+        return random_bot_move(board_state)
+    else:
+        return hard_bot_move(board_state, bot_symbol)
